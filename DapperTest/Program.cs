05eb54a72660ef342eb492106cacea4e69d67e03
James Spinella <jspinella@jatsby.com>
2020-07-22 13:52:31 -0400

    initial commit

---
+++ DapperTest/Program.cs
@@ -0,0 +1,58 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+
+namespace DapperTest
+{
+    class Program
+    {
+        public static readonly string CONNSTRING = "Server=localhost; Database=Test; Trusted_Connection=True;";
+
+        static void Main(string[] args)
+        {
+            DatabaseOperations();
+        }
+
+        private static void DatabaseOperations()
+        {
+            var tasks = new List<Task>();
+
+            tasks.AddRange(new List<Task> {
+                InsertAsync(),
+                ResetAsync(),
+            });
+
+            Task.WhenAll(tasks);
+            //await ResetAsync();
+        }
+
+        public static Task InsertAsync()
+        {
+            //var sql = @"INSERT INTO Test VALUES (1,'John'),(2,'James')";
+            var sql = "some invalid sql";
+
+            using (var connection = new SqlConnection(CONNSTRING))
+                return connection.ExecuteAsync(sql);
+        }
+
+        public static Task ResetAsync()
+        {
+            //var sql = @"DELETE FROM Test";
+            var sql = "some invalid sql";
+
+            using (var connection = new SqlConnection(CONNSTRING))
+                return connection.ExecuteAsync(sql);
+        }
+
+        public static void Reset()
+        {
+            //var sql = @"DELETE FROM Test";
+            var sql = "some invalid sql";
+
+            using (var connection = new SqlConnection(CONNSTRING))
+                connection.Execute(sql);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # DapperTest
- Testing out Dapper's ExecuteAsync
+ Testing out Dapper's ExecuteAsync.
+
+ ## Setup
+ 0. Install the latest SQL Server on local computer
+ 1. Create new Database and Table, both named "Test"
+
+`CREATE TABLE [dbo].[Test](
+	[ID] [int] NOT NULL,
+	[Name] [varchar](50) NOT NULL
+) ON [PRIMARY]`
